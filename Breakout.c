2391e7c7e37bf2effcb051f81cd9fec902a6c29a
DannyMu4 <dmurillo@uoguelph.ca>
2022-10-25 08:56:46 -0400

    added Breakout.c File

---
+++ Breakout.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) 
+{
+   unsigned int x;
+   for(x=1; x<=10;x++)
+   {
+      if(x==5){
+         break;
+      }
+      printf("%u", x);
+   }
+printf("\nBroke Out of loop at X == %u\n", x);
+return 0;
+}
